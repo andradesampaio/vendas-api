6011a81c51a24a30020a3ca732adfee2beff1675
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-04-04 12:29:05 -0300

    Autorização de URL's 2

--- src/main/java/com/github/carreiras/vendasapi/config/SecurityConfig.java
+++ src/main/java/com/github/carreiras/vendasapi/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/api/clientes/**").permitAll()
+                .authorizeRequests().antMatchers("/api/clientes/**").authenticated()
                 .and()
                 .formLogin();
 
